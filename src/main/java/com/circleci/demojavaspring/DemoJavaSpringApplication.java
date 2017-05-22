34bc2c00a70cb56cec6bc291f1821d404bbc3b18
Lev Lazinskiy <lev@levlaz.org>
2017-05-22 16:21:34 -0700

    spring init

---
+++ src/main/java/com/circleci/demojavaspring/DemoJavaSpringApplication.java
@@ -0,0 +1,12 @@
+package com.circleci.demojavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJavaSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/circleci/demojavaspring/DemoJavaSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.circleci.demojavaspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoJavaSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
