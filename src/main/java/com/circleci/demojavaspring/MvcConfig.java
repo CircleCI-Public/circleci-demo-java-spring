c3f6e4b9d6cb4a88aabc8edec1ba538b242a0f6a
Lev Lazinskiy <lev@levlaz.org>
2017-05-22 19:27:32 -0700

    Add bootstrap, update security

---
+++ src/main/java/com/circleci/demojavaspring/MvcConfig.java
@@ -0,0 +1,17 @@
+package com.circleci.demojavaspring;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("index");
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/register").setViewName("register");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
