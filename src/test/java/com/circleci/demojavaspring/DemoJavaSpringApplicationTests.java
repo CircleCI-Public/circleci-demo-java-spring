cef8e4e7f2c13de0b50ef7783564e67a90b2bf9e
Lev Lazinskiy <lev@levlaz.org>
2017-05-23 15:59:27 -0700

    use test.properties file

--- src/test/java/com/circleci/demojavaspring/DemoJavaSpringApplicationTests.java
+++ src/test/java/com/circleci/demojavaspring/DemoJavaSpringApplicationTests.java
@@ -3,10 +3,14 @@ package com.circleci.demojavaspring;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration
+@TestPropertySource("/test.properties")
 public class DemoJavaSpringApplicationTests {
 
 	@Test
