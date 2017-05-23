5a96105171f1b3d9a659601aabfadbe7f3f71e9a
Lev Lazinskiy <lev@levlaz.org>
2017-05-23 16:02:39 -0700

    switch generation strategy
    attempt to resolve `ERROR:  sequence "hibernate_sequence" does not exist` error.

--- src/main/java/com/circleci/demojavaspring/model/Author.java
+++ src/main/java/com/circleci/demojavaspring/model/Author.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 @Entity
 public class Author {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
 
     private String name;
--- src/main/java/com/circleci/demojavaspring/model/Quote.java
+++ src/main/java/com/circleci/demojavaspring/model/Quote.java
@@ -10,7 +10,7 @@ import javax.persistence.ManyToOne;
 @Entity
 public class Quote {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
 
     private Integer personId;
