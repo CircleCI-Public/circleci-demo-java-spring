c0e53979f74f40522dab430cbcfd75c239323784
jovinod <37402800+jovinod@users.noreply.github.com>
2018-07-13 14:40:12 +0530

    Update DemoJavaSpringApplicationTests.java

--- src/test/java/com/circleci/demojavaspring/DemoJavaSpringApplicationTests.java
+++ src/test/java/com/circleci/demojavaspring/DemoJavaSpringApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ContextConfiguration
